4f81f3a15b31699470a88ab35efd132f0354e4ed
Y.V.S.LAKSHMAN <bunnylakshman1@gmail.com>
2025-02-04 16:08:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
+const bcrypt = require('bcryptjs'); // Using bcryptjs instead of bcrypt
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
